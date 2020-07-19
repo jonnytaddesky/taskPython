309f0e32cc39a1384f65f2a30ac9feeaf5e2d249
jonnytaddesky <s.v.artem2020@gmail.com>
2020-07-19 13:10:13 +0300

    function

--- func.py
+++ func.py
@@ -1,18 +1,17 @@
-import math
-import itertools
-import numpy
+import numpy as np
 
-# def step(x, m):
-#     x = 1
-#     m = 1
-#     result = 1
-#     for t in range(m):
-#         result *= x
-#         return result
+def calc(x, m):
+    result = 1
+    for j in np.arange(m):
+        result *= x
+    return result    
 
+def power():
+    z = 0
+    for x in np.arange(-1.1, 0.3, 0.2):
+        for m in np.arange(1, 5, 1):
+            z = calc(x, m)*calc(np.sin(x*m),m)
+            print('Значение функции = {} При m = {}'.format(z, m)) 
+        print('Вычислено при x = {}'.format(x))
 
-# def func():
-x1, x2, dx = -1.1, 0.3, 0.2
-m1, m2, dm = 1, 5, 1
-for i, j in itertools.product(range(-1.1, 0.3, 0.2), range(1, 5, 1)):
-    print(i + j)
+power()
